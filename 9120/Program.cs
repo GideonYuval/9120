5b1d4f48c19463e92c7a9bf4866f30450121ddb9
GideonYuval <72265313+giladyuval@users.noreply.github.com>
2024-10-06 19:03:54 +0300

    fixed questions, alligned with solution

--- 9120/Program.cs
+++ 9120/Program.cs
@@ -12,13 +12,15 @@ namespace _9120
         {
         }
 
+
+
         // 1
         // sum of elements in list
         // param : Node<int>
         // return : int
         // example: param: ->1->2->3, return: 6
         // example: param: ->10->20->30, return: 60
-        public static int sum(Node<int> lst)
+        public static int Sum(Node<int> lst)
         {
             // Your implementation here
             return 0;
@@ -30,10 +32,13 @@ namespace _9120
         // return : int
         // example: param: ->1->2->3, return: 3
         // example: param: ->10->20->30, return: 30
-        public static int findMax(Node<int> lst)
+        public static int FindMax(Node<int> lst)
         {
             // Your implementation here
+            int max = lst.GetValue();
             return 0;
+
+
         }
 
         // 3
@@ -43,9 +48,10 @@ namespace _9120
         // return : IntNode
         // example: param1: ->1->2->3, param2: 2, return: Node with value 2
         // example: param1: ->10->20->30, param2: 20, return: Node with value 20
-        public static IntNode FindInList(IntNode first, int lookFor)
+        // example: param1: ->10->20->30, param2: 40, return: null
+        public static Node<int> FindInList(Node<int> first, int lookFor)
         {
-            // Your implementation here
+
             return null;
         }
 
@@ -56,23 +62,26 @@ namespace _9120
         // return : bool
         // example: param1: ->1->2->3, param2: 2, return: true
         // example: param1: ->10->20->30, param2: 25, return: false
-        public static bool IsInList(IntNode first, int lookFor)
+        public static bool IsInList(Node<int> first, int lookFor)
         {
-            // Your implementation here
+
             return false;
         }
 
         // 5
         // insert an element at a specific position in the list
         // param1 : Node<int>
-        // param2 : int (position)
+        // param2 : int (position, from 1, legal value)
         // param3 : int (value)
         // return : Node<int>
-        // example: param1: ->1->2->4, param2: 2, param3: 3, return: ->1->2->3->4
-        // example: param1: ->1->3->4, param2: 1, param3: 2, return: ->1->2->3->4
-        public static Node<int> insertAtPosition(Node<int> lst, int position, int value)
+        // example: param1: ->1->2->4, param2: 3, param3: 3, return: ->1->2->3->4
+        // example: param1: ->1->3->4, param2: 2, param3: 2, return: ->1->2->3->4
+        // example: param1: ->1->3->4, param2: 1, param3: 7, return: ->7->2->3->4
+
+
+        public static Node<int> InsertAtPosition(Node<int> lst, int position, int value)
         {
-            // Your implementation here
+
             return lst;
         }
 
@@ -82,10 +91,11 @@ namespace _9120
         // param2 : int (position)
         // return : Node<int>
         // example: param1: ->1->2->3->4, param2: 2, return: ->1->3->4
-        // example: param1: ->1->2->3->4, param2: 1, return: ->1->3->4
-        public static Node<int> removeAtPosition(Node<int> lst, int position)
+        // example: param1: ->1->2->3->4, param2: 1, return: ->2->3->4
+        public static Node<int> RemoveAtPosition(Node<int> lst, int position)
         {
-            // Your implementation here
+
+
             return lst;
         }
     }
